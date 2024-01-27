86dd2d401abb099cdc8f974f8dc822449e226ca2
vedavyas-m <vedavyas.mhssv@gmail.com>
2024-01-27 23:16:10 +0530

    types of comments

---
+++ Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+    public static void main(String[] args) {
+        // single line comment
+
+        /*
+         multi
+         line
+         comment
+         */
+        System.out.println("the above shows different types of comments");
+    }
+}
